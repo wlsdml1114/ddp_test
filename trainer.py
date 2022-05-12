7c9bafe1b45862191ca62e81669247efa75ed381
wlsdml1114 <wlsdml111@gmail.com>
2022-05-12 10:57:44 +0900

    model to onnx

--- trainer.py
+++ trainer.py
@@ -31,3 +31,18 @@ ddpautoencoder = DDPAutoEncoder(data_path,batch_size)
 trainer = Trainer(max_epochs=num_epochs, gpus=num_gpus, accelerator="ddp",logger = wandb_logger)
 trainer.fit(ddpautoencoder)
 trainer.save_checkpoint(os.path.join(model_path,'./ddpautoencoder.pth'))
+
+#to onnx
+X = torch.tensor(np.zeros([128,3,680,720])).to(torch.float)
+
+torch.onnx.export(ddpautoencoder,                     # model being run
+                  ##since model is in the cuda mode, input also need to be
+                  X,              # model input (or a tuple for multiple inputs)
+                  "ddpautoencoder.onnx", # where to save the model (can be a file or file-like object)
+                  export_params=True,        # store the trained parameter weights inside the model file
+                  opset_version=10,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['input'],   # the model's input names
+                  output_names = ['output'], # the model's output names
+                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
+                                'output' : {0 : 'batch_size'}})
